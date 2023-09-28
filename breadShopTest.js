43f488a36c5448a98ee185626f9fc67a1db7abed
Ramon-Cristian Marcu <ramondotmarcu@gmail.com>
2023-09-28 16:49:54 +0300

    Add files via upload
    *removed setup duplication code*

---
+++ breadShopTest.js
@@ -0,0 +1,50 @@
+const { expect } = require("chai");
+const { beforeEach } = require("mocha");
+const { Builder, By, Select } = require("selenium-webdriver");
+
+describe('sandwich order', function() {
+    this.timeout(5000);
+    let driver;
+
+    beforeEach(async function() {
+            //setup
+            driver = await new Builder().forBrowser('chrome').build();
+            await driver.manage().setTimeouts( {implicit: 1000} );
+            await driver.get("http://localhost:4200/order/sandwich");
+            let title = await driver.getTitle();
+            expect(title).to.equal("Order a Sandwich | BreadShop");
+
+
+        })    
+    
+        it('select the bread type', async function() {
+  
+        //act
+        let ryeBreadOptionElement = await driver.findElement(By.id("bread-type-rye"));
+        await ryeBreadOptionElement.click();
+
+        //assert
+        let selectedElement = await driver.findElement(By.className("bread-type-value"));
+        let selectedValue = await selectedElement.getText();
+        expect(selectedValue).to.equal("rye bread");
+        
+        //teardown
+        await driver.quit();
+    });
+
+    it("selects the main filling", async function() {
+  
+                //act
+                let mainFillingElement = await driver.findElement(By.id("form-select-main-filling"));
+                let select = new Select(mainFillingElement);
+                await select.selectByValue("tofu");
+
+                //assert
+                let selectedElement = await driver.findElement(By.className("main-filling-value"));
+                let selectedValue = await selectedElement.getText();
+                expect(selectedValue).to.equal("tofu");
+        
+                //teardown
+                await driver.quit();
+    });
+});
